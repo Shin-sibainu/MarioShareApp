47cd1570f4b2576365635685bc0d615e18f724ef
Shintakumi <shincode0712@gmail.com>
2021-05-13 17:06:49 +0900

    staticroot fixed1

--- MarioShare/settings.py
+++ MarioShare/settings.py
@@ -108,8 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/sta/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 MEDIA_ROOT = BASE_DIR / 'media'
