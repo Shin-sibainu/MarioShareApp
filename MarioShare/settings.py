e457fce7e1cc6ce84ee44991a8ef0a4a48f87045
Shintakumi <shincode0712@gmail.com>
2021-05-14 10:06:27 +0900

    final heroku deploy

--- MarioShare/settings.py
+++ MarioShare/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # 本番環境ならこっちが必要？
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 # 開発環境で調べたいならここは必要。
-STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+# STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
